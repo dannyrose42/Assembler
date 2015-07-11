e9cdaedee9d20ec17711a0fb4947ee30602afef8
Danny Rose <dannyrose42@gmail.com>
2015-07-11 14:58:19 -0500

    Moved .asm and .hack files into new testFiles folder

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW_1-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW_1-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=hack_assembler
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW_1-Windows/hack_assembler
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW_1-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/hack_assembler
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=hackassembler.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW_1-Windows/package/hackassembler.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/hackassembler.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW_1-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/MinGW_1-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=hack_assembler
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW_1-Windows/hack_assembler
-CND_PACKAGE_DIR_Release=dist/Release/MinGW_1-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/hack_assembler
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=hackassembler.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW_1-Windows/package/hackassembler.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/hackassembler.tar
 #
 # include compiler specific variables
 #
